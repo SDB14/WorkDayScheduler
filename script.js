3e3ed1b454b9d23d5688ea7404e87cd490c6e1e6
SDB14 <cwruboot22@gmail.com>
2021-09-22 19:06:05 -0400

    final notes

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ localStorage.setItem(timeDiv,textvalue)
 
 // in order to reference key (time block) make key and string value equal to keep on screen reload (persistance)
 $("#9 .textvalue").val(localStorage.getItem("9"))
-
+// repeat for each hour id
 // set function to change color for the hour
 
 function updateCSS(){
     var timeSelector=moment().hours()
 
     $(".time-block").each(function(){
-
+// might have to wrap $(this).attr("id") in a parse int
         var hourDiv=$(this).attr("id")
 
         if (hourDiv < timeSelector){
@@ -28,10 +28,15 @@ function updateCSS(){
         }
 
         else if (hourDiv > timeSelector){
+        $(this).removeClass("past present")
+        
+        
             $(this).addClass("future")
         }
 
         else {
+           $(this).removeClass("past future")           
+           
             $(this).addClass("present")
         }
 
@@ -40,3 +45,5 @@ function updateCSS(){
     })
 
 }
+
+updateCSS()
