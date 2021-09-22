962dcd65d73ff4d2bb897c29e94f26b0c5eab612
SDB14 <cwruboot22@gmail.com>
2021-09-22 18:14:39 -0400

    save local storage on reload

--- script.js
+++ script.js
@@ -11,3 +11,6 @@ localStorage.setItem(timeDiv,textvalue)
 
 })
 
+// in order to reference key (time block) make key and string value equal to keep on screen reload (persistance)
+$("#9 .textvalue").val(localStorage.getItem("9"))
+
