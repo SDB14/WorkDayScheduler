5587fe8d1ac0054522502515b7fbbf805f3dc50a
SDB14 <cwruboot22@gmail.com>
2021-09-22 21:56:08 -0400

    added storage code for hour ids

--- script.js
+++ script.js
@@ -12,7 +12,20 @@ localStorage.setItem(timeDiv,textvalue)
 })
 
 // in order to reference key (time block) make key and string value equal to keep on screen reload (persistance)
+$("#7 .textvalue").val(localStorage.getItem("7"))
+$("#8 .textvalue").val(localStorage.getItem("8"))
 $("#9 .textvalue").val(localStorage.getItem("9"))
+$("#10 .textvalue").val(localStorage.getItem("10"))
+$("#11 .textvalue").val(localStorage.getItem("11"))
+$("#12 .textvalue").val(localStorage.getItem("12"))
+$("#13 .textvalue").val(localStorage.getItem("13"))
+$("#14 .textvalue").val(localStorage.getItem("14"))
+$("#15 .textvalue").val(localStorage.getItem("15"))
+$("#16 .textvalue").val(localStorage.getItem("16"))
+$("#17 .textvalue").val(localStorage.getItem("17"))
+$("#18 .textvalue").val(localStorage.getItem("18"))
+$("#19 .textvalue").val(localStorage.getItem("19"))
+$("#20 .textvalue").val(localStorage.getItem("20"))
 // repeat for each hour id
 // set function to change color for the hour
 
